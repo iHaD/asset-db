8ccbc9caa4fbe4a2104ff24bd0a89fdba0cecaa0
Johnny Wu <KarasAya@gmail.com>
2015-06-13 01:47:36 +0800

    more test

--- test/meta.js
+++ test/meta.js
@@ -1,17 +1,23 @@
 var Path = require('fire-path');
 
 var AssetMeta = require('../lib/meta/asset');
+var FolderMeta = require('../lib/meta/folder');
 var Meta = require('../lib/meta');
 var JS = require('../lib/js-utils.js');
 JS.mixin( Meta, require('../lib/utils') );
 
 function PngMeta () {}
 function ImageMeta () {}
+function AtlasFolderMeta () {}
+function AtlasMeta () {}
 
 describe('Meta.findCtor', function () {
     before(function ( done ) {
         Meta.register('.png', null, false, JS.extend(ImageMeta,AssetMeta));
         Meta.register('.png', '**/particles/*.png', false, JS.extend(PngMeta,AssetMeta));
+
+        Meta.register('.atlas', null, true, JS.extend(AtlasFolderMeta,AssetMeta));
+        Meta.register('.atlas', null, false, JS.extend(AtlasMeta,AssetMeta));
         done();
     });
     after(function ( done ) {
@@ -28,6 +34,30 @@ describe('Meta.findCtor', function () {
     it('should get ImageMeta', function ( done ) {
         var ctor = Meta.findCtor( Path.join( __dirname, 'fixtures/example-assets/export/spineboy/spineboy.png') );
         expect(ctor).to.equal(ImageMeta);
+        done();
+    });
+
+    it('should get AssetMeta', function ( done ) {
+        var ctor = Meta.findCtor( Path.join( __dirname, 'fixtures/example-assets/animation/down.asset') );
+        expect(ctor).to.equal(AssetMeta);
+        done();
+    });
+
+    it('should get FolderMeta', function ( done ) {
+        var ctor = Meta.findCtor( Path.join( __dirname, 'fixtures/example-assets/animation/') );
+        expect(ctor).to.equal(FolderMeta);
+        done();
+    });
+
+    it('should get AtlasMeta', function ( done ) {
+        var ctor = Meta.findCtor( Path.join( __dirname, 'fixtures/simple-assets-03/atlas-asset.atlas') );
+        expect(ctor).to.equal(AtlasMeta);
+        done();
+    });
+
+    it('should get AtlasFolderMeta', function ( done ) {
+        var ctor = Meta.findCtor( Path.join( __dirname, 'fixtures/simple-assets-03/atlas-folder.atlas') );
+        expect(ctor).to.equal(AtlasFolderMeta);
         done();
     });
 });
